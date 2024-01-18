194ce770658a7df35be4acae1837a07123ee9e4f
DAXXTEAM <147141400+DAXXTEAM@users.noreply.github.com>
2024-01-18 21:16:53 +0530

    Update copyright.py

--- copyright.py
+++ copyright.py
@@ -9,7 +9,7 @@ app = Client('my_bot', api_id=API_ID, api_hash=API_HASH, bot_token=BOT_TOKEN)
 
 
 #-----
-@Client.on_message(filters.command("start"))
+@app.on_message(filters.command("start"))
 def start_command(client, message):
     client.send_message(
         chat_id=message.chat.id,
@@ -20,7 +20,7 @@ def start_command(client, message):
 keywords_to_delete = ["www.", "https", "http" , "t.me", "NCERT", "XII", "page", "Ans", "meiotic", "divisions", "System.in", "Scanner", "void", "nextInt"]
 #-----------------------------------------------------------------------------
 #-----------------------------------------------------------------------------
-@Client.on_message(filters.group & filters.text & ~filters.me)
+@app.on_message(filters.group & filters.text & ~filters.me)
 async def delete_links(client, message):
     if any(keyword in message.text for keyword in keywords_to_delete) and len(message.text.split()) > 20:
         await message.delete()
@@ -28,20 +28,20 @@ async def delete_links(client, message):
 #-------------------------------------------------------------------------------
 #-------------------------------------------------------------------------------
 
-@Client.on_edited_message(filters.group & filters.text & ~filters.me)
+@app.on_edited_message(filters.group & filters.text & ~filters.me)
 async def delete_edited_links(client, edited_message):
     if any(keyword in edited_message.text for keyword in keywords_to_delete) and len(edited_message.text.split()) > 20:
         await edited_message.delete()
 #------------------------------------------------------------------------------------
 #------------------------------------------------------------------------------------
 
-@Client.on_message(filters.group & filters.text & ~filters.me)
+@app.on_message(filters.group & filters.text & ~filters.me)
 async def delete_long_messages(client, message):
     if len(message.text.split()) >= 20:
         await message.delete()
 #----------------------------------------------------------------------------------------
 #----------------------------------------------------------------------------------------
-@Client.on_edited_message(filters.group & filters.text & ~filters.me)
+@app.on_edited_message(filters.group & filters.text & ~filters.me)
 async def delete_edited_long_messages(client, edited_message):
     if len(edited_message.text.split()) >= 20:
         await edited_message.delete()
@@ -50,4 +50,4 @@ async def delete_edited_long_messages(client, edited_message):
 #-------------------------------------------------------------------------------------------
 #-------------------------------------------------------------------------------------------
 
-Client.run()
+app.run()
